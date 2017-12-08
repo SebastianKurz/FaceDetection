8ef8044c33c945fbbb6da3dedad7d296a482ac58
Unknown <sebastiankurz4@gmail.com>
2017-12-08 17:32:06 +0100

    Intersection over Union applied on the evaluation of all images
    * TODO: find a performant method for determining if the boxes overlap or not

--- Code/Evaluation.py
+++ Code/Evaluation.py
@@ -1,6 +1,6 @@
 import FaceDetection
 import ImageLoader
-import IntersectionOverUnion as IOU
+import IntersectionOverUnion as IoU
 import cv2
 import numpy as np
 from pprint import pprint
@@ -8,7 +8,8 @@ from pprint import pprint
 
 def evaluate():
     imgs = ImageLoader.gen_load_imgs("Data/wider_face_train_bbx_gt.txt")
-    union_area_list = []
+    intersection_sum = 0
+    union_sum = 0
 
     for i in imgs:
         img = i.get("img", None)
@@ -16,7 +17,7 @@ def evaluate():
 
         faces = FaceDetection.detect_faces(img)
         FaceDetection.draw_boxes(img, faces, (255, 0, 0))
-        
+
         for position in positions:
             x_actual = position.get("x", None)
             y_actual = position.get("y", None)
@@ -29,8 +30,24 @@ def evaluate():
                 (x_actual + w_actual, y_actual + h_actual),
                 (0,255,0)
             )
+
+            #IoU
+            for face in faces:
+                x, y, w, h = face
+                calculated_position = {"x": x, "y": y, "width": w, "height": h}
+                common_points = IoU.points_in_intersection_area(position, calculated_position)
+                if (len(common_points) > 0):
+                    intersection_sum += IoU.intersection_area_sum(position, calculated_position)
+                    union_sum += IoU.union_area_sum(position, calculated_position)
+
+        #intersection_over_union = intersection_sum / union_sum
+        #print(intersection_over_union)
         cv2.imshow("pictures", img)
         cv2.waitKey(1000)
 
+    intersection_over_union = intersection_sum / union_sum
+    print(intersection_over_union)
 
 evaluate()
+
+#TODO: find a performant method for determining if the boxes overlap or not
